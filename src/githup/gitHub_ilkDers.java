321d6f089027efb35b404472046ce787f9bf3716
YusufDogan <yusufdogan0408@gmail.com>
2025-04-19 00:26:56 +0200

    ilk ders basarali oldu

---
+++ src/githup/gitHub_ilkDers.java
@@ -0,0 +1,9 @@
+package githup;
+
+public class gitHub_ilkDers {
+    public static void main(String[] args) {
+        System.out.println("Hello Github");
+
+        System.out.println("Hos geldin");
+    }
+}
